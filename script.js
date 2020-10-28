5aa920437cf63885d7d30db1bcd729f37e538518
chamben <63270695+chamben@users.noreply.github.com>
2020-10-28 01:19:10 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+ // JAVASCRIPT FUNCTIONS
+function scrollToAbout() {
+
+    if ($('.section1').length != 0) {
+      $("html, body").animate({
+        scrollTop: $('.section1').offset().top
+      }, 1000);
+    }
+  }
+function scrollToServices() {
+
+if ($('.section2').length != 0) {
+  $("html, body").animate({
+    scrollTop: $('.section2').offset().top
+  }, 1000);
+}
+}
+function scrollToProjects() {
+
+if ($('.section3').length != 0) {
+  $("html, body").animate({
+    scrollTop: $('.section3').offset().top
+  }, 1000);
+}
+}
+ var i=0;
+    	function read(){
+    		if (!i) {
+    			document.getElementById("more").style.display = "inline";
+    			document.getElementById("dots").style.display = "none";
+    			document.getElementById("read").innerHTML = "Read Less";
+    			i=1;
+    		}
+    		else{
+    			document.getElementById("more").style.display = "none";
+    			document.getElementById("dots").style.display = "inline";
+    			document.getElementById("read").innerHTML = "Read More";
+    			i=0;
+    		}
+    	}
